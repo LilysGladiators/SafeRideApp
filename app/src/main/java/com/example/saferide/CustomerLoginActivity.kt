d8e2dacc48832098d37605b3117766a1b2baf87f
Lily Eli Goloh <lilygoloh@icloud.com>
2024-04-15 08:21:52 -0500

    ActivityCustomerLogin correction

--- app/src/main/java/com/example/saferide/CustomerLoginActivity.kt
+++ app/src/main/java/com/example/saferide/CustomerLoginActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class CustomerLoginActivty : AppCompatActivity() {
