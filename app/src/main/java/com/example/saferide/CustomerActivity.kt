6bce3d72c615ffe2b36b30dc28ff560a0ca471cd
shaseth <131016800+shaseth@users.noreply.github.com>
2024-05-01 15:22:22 -0500

    Implemented navigation drawer into DriverActivity. Still need to touch up features later

--- app/src/main/java/com/example/saferide/CustomerActivity.kt
+++ app/src/main/java/com/example/saferide/CustomerActivity.kt
@@ -28,7 +28,7 @@ class CustomerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_customer)
 
         // Need to create some sort of username creation system likely?
-        //val user = FirebaseAuth.getInstance().currentUser.toString()
+        val user = FirebaseAuth.getInstance().currentUser?.displayName.toString()
         val email = FirebaseAuth.getInstance().currentUser?.email.toString()
 
         val item1 = PrimaryDrawerItem().apply { nameRes = R.string.drawer_item_home; identifier = 1 }
@@ -40,7 +40,7 @@ class CustomerActivity : AppCompatActivity() {
         slider.headerView = AccountHeaderView(this).apply {
             attachToSliderView(slider)
             addProfiles(
-                ProfileDrawerItem().apply { nameText = "USERNAME"; descriptionText = email; iconRes = R.drawable.ic_launcher_foreground; identifier = 102}
+                ProfileDrawerItem().apply { nameText = user; descriptionText = email; iconRes = R.drawable.ic_launcher_foreground; identifier = 102}
             )
             onAccountHeaderListener = { view, profile, current ->
                 //react to profile changes
--- app/src/main/java/com/example/saferide/DriverActivity.kt
+++ app/src/main/java/com/example/saferide/DriverActivity.kt
@@ -2,14 +2,74 @@ package com.example.saferide
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.descriptionText
+import com.mikepenz.materialdrawer.model.interfaces.iconRes
+import com.mikepenz.materialdrawer.model.interfaces.nameRes
+import com.mikepenz.materialdrawer.model.interfaces.nameText
+import com.mikepenz.materialdrawer.widget.AccountHeaderView
+import com.mikepenz.materialdrawer.widget.MaterialDrawerSliderView
 
 class DriverActivity : AppCompatActivity() {
+
+    private lateinit var slider: MaterialDrawerSliderView
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_driver)
+
+        val user = FirebaseAuth.getInstance().currentUser?.displayName.toString()
+        val email = FirebaseAuth.getInstance().currentUser?.email.toString()
+
+        val item1 = PrimaryDrawerItem().apply { nameRes = R.string.drawer_item_home; identifier = 1 }
+        val item2 = SecondaryDrawerItem().apply { nameRes = R.string.drawer_item_order; identifier = 2 }
+        val item3 = SecondaryDrawerItem().apply { nameRes = R.string.drawer_item_eta; identifier = 3 }
+
+        slider = findViewById(R.id.slider)
+
+        slider.headerView = AccountHeaderView(this).apply {
+            attachToSliderView(slider)
+            addProfiles(
+                ProfileDrawerItem().apply { nameText = user; descriptionText = email; iconRes = R.drawable.ic_launcher_foreground; identifier = 102}
+            )
+            onAccountHeaderListener = { view, profile, current ->
+                //react to profile changes
+                false
+            }
+            withSavedInstance(savedInstanceState)
+        }
+
+        slider.itemAdapter.add(
+            item1,
+            DividerDrawerItem(),
+            item2,
+            item3
+        )
+
+        slider.setSelection(2)
+
+        button = findViewById(R.id.navigationButton)
+        button.setOnClickListener {
+            slider.drawerLayout?.openDrawer(slider)
+        }
+
+        slider.onDrawerItemClickListener = { _, drawerItem, _ ->
+            // do something with clicked item :D
+            when (drawerItem) {
+                //item1 -> startHomeActivity()
+                //item2 -> insert here
+                //item3 -> startMapActivity()
+            }
+            false
+        }
 
         // Initialize buttons
         val buttonAcceptRide = findViewById<Button>(R.id.buttonAcceptRide)
